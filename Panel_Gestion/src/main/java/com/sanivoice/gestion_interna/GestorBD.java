87aa084218d80bf0c95c1b910666039bf9e2ceec
920486 <920486@vbox>
2023-05-06 21:32:31 +0200

    Registro

--- Panel_Gestion/src/main/java/com/sanivoice/gestion_interna/GestorBD.java
+++ Panel_Gestion/src/main/java/com/sanivoice/gestion_interna/GestorBD.java
@@ -7,62 +7,59 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 
-
-
 /**
  * Clase que gestiona la conexión a la base de datos
+ * 
  * @author Equipo de SaniVoice
  *
  */
 public class GestorBD {
 	private Connection conexion;
 	private static GestorBD gbd;
+
 	private GestorBD() {
 		try {
 			Class.forName("com.mysql.cj.jdbc.Driver");
-		}catch(Exception ex) {
+		} catch (Exception ex) {
 			System.err.println("ERROR BUSCANDO LA CLASE");
 			ex.printStackTrace();
 		}
 	}
-	
+
 	public static GestorBD getGestorBD() {
-		if(gbd==null) {
+		if (gbd == null) {
 			gbd = new GestorBD();
 		}
 		return gbd;
 	}
-	
-	
+
 	private void conectar() {
-		 try{
-			 	String usuario = "TU_USUARIO";
-			 	String clave = "CLAVE";
-			 	String puerto = "8889";
-			 	String nombreDB = "database";
-			 	conexion = DriverManager.getConnection("jdbc:mysql://localhost:"+puerto+"/"+nombreDB, "admin", "test");
-	        }catch (SQLException ex) {
-	            ex.printStackTrace(); 
-	        }
+		try {
+			String usuario = "TU_USUARIO";
+			String clave = "CLAVE";
+			String puerto = "8889";
+			String nombreDB = "database";
+			conexion = DriverManager.getConnection("jdbc:mysql://localhost:" + puerto + "/" + nombreDB, "admin",
+					"test");
+		} catch (SQLException ex) {
+			ex.printStackTrace();
+		}
 	}
-	
+
 	private void desconectar() {
-		try{
+		try {
 			conexion.close();
-		}catch(Exception ex) {
+		} catch (Exception ex) {
 			ex.getStackTrace();
 		}
 	}
-	
+
 	/**
-	 * TODO
-	 * ESCRIBIR MÉTODOS PARA ALMACENAR LOS DATOS NECESARIOS EN 
-	 * LA BASE DE DATOS DE SANIVOICE GESTIÓN (EL .SQL QUE ESTÁ CON EL RESTO
-	 * DE DOCUMENTOS)
-	 * Por ejemplo, guardarUsuario(Usuario u) y dentro realizar la consulta.
+	 * TODO ESCRIBIR MÉTODOS PARA ALMACENAR LOS DATOS NECESARIOS EN LA BASE DE DATOS
+	 * DE SANIVOICE GESTIÓN (EL .SQL QUE ESTÁ CON EL RESTO DE DOCUMENTOS) Por
+	 * ejemplo, guardarUsuario(Usuario u) y dentro realizar la consulta.
 	 */
-	
-	
+
 	/**
 	 * Método para almacenar datos
 	 */
@@ -70,19 +67,18 @@ public class GestorBD {
 		conectar();
 		int regAfectados;
 		String dml = "";
-        try {
-            dml = "insert into Municipio(id, nombre, habitantes) values "
-                    + "(?, ?, ?)";
-            PreparedStatement sentenciaPreparada = conexion.prepareStatement(dml);
-            sentenciaPreparada.setInt(1, 9);
-            sentenciaPreparada.setString(2, "Amorebieta");
-            sentenciaPreparada.setInt(3, 120);
-            regAfectados = sentenciaPreparada.executeUpdate();
-            sentenciaPreparada.close();
-            
-        } catch (SQLException ex) {
-            ex.printStackTrace();
-        }
+		try {
+			dml = "insert into Municipio(id, nombre, habitantes) values " + "(?, ?, ?)";
+			PreparedStatement sentenciaPreparada = conexion.prepareStatement(dml);
+			sentenciaPreparada.setInt(1, 9);
+			sentenciaPreparada.setString(2, "Amorebieta");
+			sentenciaPreparada.setInt(3, 120);
+			regAfectados = sentenciaPreparada.executeUpdate();
+			sentenciaPreparada.close();
+
+		} catch (SQLException ex) {
+			ex.printStackTrace();
+		}
 		desconectar();
 	}
 
@@ -93,22 +89,46 @@ public class GestorBD {
 		conectar();
 		try {
 			Statement sentencia = conexion.createStatement();
-	        String dql = "select * from Municipio";
-	        ResultSet resultado = sentencia.executeQuery(dql);
-	        while (resultado.next()) {
-	            
-	            int id = resultado.getInt("id");
-	            String nombre = resultado.getString("nombre");
-	            int numHab = resultado.getInt("habitantes");
-	            System.out.println("ID: "+id+". Nombre: "+nombre+". NumHab: "+numHab);
-	        }
-	        resultado.close();           
-	        sentencia.close();
-		}catch(Exception ex) {
+			String dql = "select * from Municipio";
+			ResultSet resultado = sentencia.executeQuery(dql);
+			while (resultado.next()) {
+
+				int id = resultado.getInt("id");
+				String nombre = resultado.getString("nombre");
+				int numHab = resultado.getInt("habitantes");
+				System.out.println("ID: " + id + ". Nombre: " + nombre + ". NumHab: " + numHab);
+			}
+			resultado.close();
+			sentencia.close();
+		} catch (Exception ex) {
 			ex.printStackTrace();
 		}
-		
+
 		desconectar();
 	}
-	
+
+	public void getRegistroCorrecto(Paciente p) {
+		conectar();
+		int regAfectados;
+		String dml = "";
+		try {
+			dml = "insert into Paciente(id, nombre, apellidos, dni, tarjeta_sanitaria, telefono, domicilio, correo_electronico, contraseña) values " + "(?, ?, ?, ?, ?, ?, ?, ?, ?)";
+			PreparedStatement sentenciaPreparada = conexion.prepareStatement(dml);
+			sentenciaPreparada.setString(1, p.getNombre());
+			sentenciaPreparada.setString(2, p.getApellidos());
+			sentenciaPreparada.setString(3, p.getDNI());
+			sentenciaPreparada.setString(4, p.getTarjetaSanitaria());
+			sentenciaPreparada.setString(5, p.getTelContacto());
+			sentenciaPreparada.setString(6, p.getDomicilio());
+			sentenciaPreparada.setString(7, p.getEmail());
+			sentenciaPreparada.setString(8, p.getClave());
+			regAfectados = sentenciaPreparada.executeUpdate();
+			sentenciaPreparada.close();
+
+		} catch (SQLException ex) {
+			ex.printStackTrace();
+		}
+		desconectar();
+	}
+
 }
--- Panel_Gestion/src/main/webapp/registro.jsp
+++ Panel_Gestion/src/main/webapp/registro.jsp
@@ -1,11 +1,10 @@
 
 <%
-
 //Para mandar redirecciones desde JSP es así
 //response.sendRedirect("http://google.es");
 %>
 <%@ page language="java" contentType="text/html; charset=ISO-8859-1"
-    pageEncoding="ISO-8859-1"%>
+	pageEncoding="ISO-8859-1"%>
 <!DOCTYPE html>
 <html>
 <head>
@@ -14,6 +13,23 @@
 </head>
 <body>
 	<h1>REGISTRO</h1>
-	Esta es la vista de registro. Decórala como más te parezca.
+	<form method="post" action="RegistroServlet">
+		<label for="nombre">Nombre:</label> 
+		<input type="text" id="nombre" name="nombre"> 
+		<label for="apellidos">Apellidos:</label> 
+		<input type="text" id="apellidos" name="apellidos"> 
+		<label for="dni">DNI:</label>
+		<input type="text" id="dni" name="dni"> 
+		<label for="tarjeta_sanitaria">Tarjeta sanitaria:</label> 
+		<input type="text" id="tarjeta_sanitaria" name="tarjeta_sanitaria"> 
+		<label for="telefono">Teléfono:</label> 
+		<input type="text" id="telefono" name="telefono"> 
+		<label for="domicilio">Domicilio:</label>
+		<input type="text" id="domicilio" name="domicilio"> 
+		<label for="correo_electronico">Correo electrónico:</label> 
+		<input type="text" id="correo_electronico" name="correo_electronico"> 
+		<label for="contraseña">Contraseña:</label> 
+		<input type="text" id="contraseña" name="contraseña">
+	</form>
 </body>
 </html>

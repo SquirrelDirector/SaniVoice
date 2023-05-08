f9ec17c0b54d3cecc36e20a075d3940c1d9d1602
Christian <56068195+SquirrelDirector@users.noreply.github.com>
2023-05-08 02:23:00 +0200

    Magia Disney en Conector

--- Panel_Gestion/src/main/java/com/sanivoice/conector_centros_salud/ConectorCentroNoAdscrito.java
+++ Panel_Gestion/src/main/java/com/sanivoice/conector_centros_salud/ConectorCentroNoAdscrito.java
@@ -1,12 +1,14 @@
 package com.sanivoice.conector_centros_salud;
 
 import java.io.ByteArrayInputStream;
+import java.io.IOException;
 import java.io.InputStream;
 import java.io.StringReader;
 import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpClient.Version;
 import java.net.http.HttpRequest;
+import java.net.http.HttpRequest.BodyPublishers;
 import java.net.http.HttpResponse;
 import java.net.http.HttpResponse.BodyHandlers;
 import java.util.ArrayList;
@@ -45,16 +47,100 @@ public class ConectorCentroNoAdscrito extends Conector {
 
 	@Override
 	public ArrayList<String> obtenerFechasDisponibles(Paciente p, Facultativo f) {
-		return null;
+		ArrayList<String> fechas = new ArrayList<String>();
+		HttpClient client = HttpClient.newHttpClient();
+		HttpRequest request = HttpRequest.newBuilder()
+							.uri(URI.create(URL+"/getFechasYHorasDisponibles.php?idFacultativo="+f.getNumColegiado()))
+							.GET()
+							.build();
+		HttpResponse response;
+		try {
+			response = client.send(request, BodyHandlers.ofString());
+			String datos = response.body().toString();
+			JsonReader jr = Json.createReader(new StringReader(datos));
+			JsonArray ja = jr.readArray();
+			jr.close();
+			Iterator<JsonValue> itr2 = ja.iterator();
+			while(itr2.hasNext()) {
+				JsonValue val = itr2.next();
+				JsonObject jo = val.asJsonObject();
+				String fecha = jo.getString("fecha");
+				if(!fechas.contains(fecha)) {
+					fechas.add(fecha);
+				}
+				
+				
+			}
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		
+		}
+		
+		return fechas;
 	}
 	
 	@Override
 	public ArrayList<String> obtenerHorasDisponibles(Paciente p, Facultativo f, String fecha) {
-		return null;
+		ArrayList<String> horas = new ArrayList<String>();
+		ArrayList<String> fechas = new ArrayList<String>();
+		HttpClient client = HttpClient.newHttpClient();
+		HttpRequest request = HttpRequest.newBuilder()
+							.uri(URI.create(URL+"/getFechasYHorasDisponibles.php?idFacultativo="+f.getNumColegiado()))
+							.GET()
+							.build();
+		HttpResponse response;
+		try {
+			response = client.send(request, BodyHandlers.ofString());
+			String datos = response.body().toString();
+			JsonReader jr = Json.createReader(new StringReader(datos));
+			JsonArray ja = jr.readArray();
+			jr.close();
+			Iterator<JsonValue> itr2 = ja.iterator();
+			while(itr2.hasNext()) {
+				JsonValue val = itr2.next();
+				JsonObject jo = val.asJsonObject();
+				String fechaJSON = jo.getString("fecha");
+				String horaJSON = jo.getString("hora");
+				if(fecha.equals(fechaJSON)) {
+					horas.add(horaJSON);
+				}
+				
+				
+			}
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		
+		}
+		return horas;
 	}
 	
 	@Override
 	public boolean reservarCita(Cita c) {
+		String idFac = c.getFacultativo().getNumColegiado();
+		String nombreUs = c.getPaciente().getNombre();
+		String apeUs = c.getPaciente().getApellidos();
+		String tel = c.getPaciente().getTelContacto();
+		String email = c.getPaciente().getEmail();
+		String fecha = c.getFecha();
+		String hora = c.getHora();
+		
+		try {
+			HttpClient client = HttpClient.newHttpClient();
+			String cabecera="nombre="+nombreUs+"&apellidos="+apeUs+"&tel_contacto="+tel+"&email="+email+"&fecha_hora="+fecha+"T"+hora+"&doctor="+idFac;
+			System.out.println(cabecera);
+			HttpRequest request = HttpRequest.newBuilder()
+								.uri(URI.create(URL+"/guardarCita.php"))
+								.header("Content-Type", "application/x-www-form-urlencoded")
+								.POST(BodyPublishers.ofString(cabecera))
+								.build();
+			HttpResponse response = client.send(request, BodyHandlers.ofString());
+			
+		}catch(Exception ex) {
+			ex.printStackTrace();
+		}
+		
 		return false;
 	}
 	
@@ -71,8 +157,10 @@ public class ConectorCentroNoAdscrito extends Conector {
 			ArrayList<String> especialidadesNombres= obtenerEspecialidadesCentro();
 			
 			Iterator<String> itr = especialidadesCod.iterator();
+			int idxEspec=0;
 			while(itr.hasNext()) {
 				String espe = itr.next();
+				String espeNombre = especialidadesNombres.get(idxEspec);
 				HttpClient client = HttpClient.newHttpClient();
 				HttpRequest request = HttpRequest.newBuilder()
 									.uri(URI.create(URL+"/getFacultativos.php?especialidad="+espe))
@@ -81,16 +169,16 @@ public class ConectorCentroNoAdscrito extends Conector {
 				HttpResponse response = client.send(request, BodyHandlers.ofString());
 				String datos = response.body().toString();
 				JsonReader jr = Json.createReader(new StringReader(datos));
-				JsonObject ja = jr.readObject();
+				JsonArray ja = jr.readArray();
 				jr.close();
-				System.out.println("EH");
-				/*Iterator<JsonValue> itr2 = ja.iterator();
+				Iterator<JsonValue> itr2 = ja.iterator();
 				while(itr2.hasNext()) {
 					JsonValue val = itr2.next();
-					System.out.println("itr");
-					System.out.println(val);
-				}*/
-				System.out.println("fin");
+					JsonObject jo = val.asJsonObject();
+					Facultativo f = new Facultativo(jo.getString("nombre"), jo.getString("apellidos"), jo.getString("id"), especialidadesNombres.get(idxEspec), null, null);
+					facu.add(f);
+				}
+				idxEspec++;
 			}
 			return facu;
 		}catch(Exception ex) {
